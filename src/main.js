ff7a586add9fd99dd40ec27e497d0843c77d9c1d
Your Name <you@example.com>
2019-01-07 23:03:12 +0800

    引入createAPI,将header组件样式用flex重构

--- src/main.js
+++ src/main.js
@@ -1,28 +1,12 @@
 import Vue from 'vue'
-import './cube-ui'
 import App from './App.vue'
-import VueRouter from 'vue-router'
-import VueResource from 'vue-resource'
-import goods from './components/goods/goods'
-import seller from './components/seller/seller'
-import ratings from './components/ratings/ratings'
+import './cube-ui'
+import './register'
+
 import './common/stylus/index.styl'
 
 Vue.config.productionTip = false
-Vue.use(VueRouter)
-Vue.use(VueResource)
-const routes = [
-  { path: '/', redirect: '/goods' },
-  { path: '/goods', component: goods },
-  { path: '/seller', component: seller },
-  { path: '/ratings', component: ratings }
-]
-const router = new VueRouter({
-  routes,
-  linkActiveClass: 'active'
-})
-router.go(1)
+
 new Vue({
-  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/register.js
@@ -0,0 +1,6 @@
+import { createAPI } from 'cube-ui'
+import Vue from 'vue'
+import HearderDetail from 'components/header-detail'
+
+// 用js的方式显示组件
+createAPI(Vue, HearderDetail)
