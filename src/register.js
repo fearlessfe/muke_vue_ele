cf9469d33d638248e4a0214cd4c8ad6cf226bcd2
Your Name <you@example.com>
2019-01-08 23:03:37 +0800

    新增导航组件

--- src/register.js
+++ src/register.js
@@ -1,8 +1,8 @@
-import { createAPI } from 'cube-ui'
-import Vue from 'vue'
-import HearderDetail from 'components/header-detail'
-import ShopCartList from 'components/shopcart-list'
+// import { createAPI } from 'cube-ui'
+// import Vue from 'vue'
+// import HearderDetail from 'components/header-detail'
+// import ShopCartList from 'components/shopcart-list'
 
-// 用js的方式显示组件
-createAPI(Vue, HearderDetail)
-createAPI(Vue, ShopCartList)
+// // 注册组件
+// createAPI(Vue, HearderDetail)
+// createAPI(Vue, ShopCartList)
