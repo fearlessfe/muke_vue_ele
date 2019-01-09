6edc6911014094b4983e17d50be9607bfd5af906
Your Name <you@example.com>
2019-01-09 22:48:28 +0800

    tab组件

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,80 @@
 <template>
-  <div>
+  <div id="app" @touchmove.prevent>
     <v-header :seller="seller"></v-header>
-    <div class="tab border-1px">
-      <div class="tab-item">
-        <router-link to="/goods">商品</router-link>
-      </div>
-      <div class="tab-item">
-        <router-link to="/ratings">评论</router-link>
-      </div>
-      <div class="tab-item">
-        <router-link to="/seller">商家</router-link>
-      </div>
+    <div class="tab-wrapper">
+      <tab :tabs="tabs"></tab>
     </div>
-    <router-view :seller="seller"></router-view>
   </div>
 </template>
 
 <script>
-import header from './components/v-header/v-header'
+import qs from 'query-string'
+import { getSeller } from 'api'
+import VHeader from 'components/v-header/v-header'
+import Goods from 'components/goods/goods'
+import Ratings from 'components/ratings/ratings'
+import Seller from 'components/seller/seller'
+import Tab from 'components/tab/tab'
 
-const ERR_OK = 0
-
-export default {
-  data () {
-    return {
-      seller: {}
-    }
-  },
-  created () {
-    this.$http.get('/api/seller').then((res) => {
-      res = res.body
-      if (res.errno === ERR_OK) {
-        this.seller = res.data
+  export default {
+    data() {
+      return {
+        seller: {
+          id: qs.parse(location.search).id
+        }
       }
-    })
-  },
-  components: {
-    'v-header': header
+    },
+    computed: {
+      tabs() {
+        return [
+          {
+            label: '商品',
+            component: Goods,
+            data: {
+              seller: this.seller
+            }
+          },
+          {
+            label: '评论',
+            component: Ratings,
+            data: {
+              seller: this.seller
+            }
+          },
+          {
+            label: '商家',
+            component: Seller,
+            data: {
+              seller: this.seller
+            }
+          }
+        ]
+      }
+    },
+    created() {
+      this._getSeller()
+    },
+    methods: {
+      _getSeller() {
+        getSeller({
+          id: this.seller.id
+        }).then((seller) => {
+          this.seller = Object.assign({}, this.seller, seller)
+        })
+      }
+    },
+    components: {
+      Tab,
+      VHeader
+    }
   }
-}
 </script>
 <style lang="stylus">
-  @import "./common/stylus/mixin.styl"
-    .tab
-      display flex
-      width 100%
-      height 40px
-      line-height 40px
-      border-1px(rgba(7,17,27,0.1))
-      .tab-item
-        flex 1
-        text-align center
-        & > a
-          display block
-          font-size 14px
-          color rgb(77,85,93)
-          &.active
-            color rgb(240,20,20)
+  #app
+    .tab-wrapper
+      position: fixed
+      top: 136px
+      left: 0
+      right: 0
+      bottom: 0
 </style>
